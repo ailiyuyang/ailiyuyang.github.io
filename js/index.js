3fc6430743a9d6a821790ec3edab1dd94b61f283
ailiyuyang <42561687+ailiyuyang@users.noreply.github.com>
2018-12-23 20:13:36 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,10 +10,10 @@ let timer = setInterval(function() {
   const diff = today - loveDay;
 
   // math
-  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  let days = 299;
+  let hours = 10;
+  let minutes = 13;
+  let seconds = 37;
 
   // display
   document.getElementById("timer").innerHTML =
