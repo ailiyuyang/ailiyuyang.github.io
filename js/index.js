4b86015c8fc478b99d8d509b4dae1639343630b2
ailiyuyang <42561687+ailiyuyang@users.noreply.github.com>
2018-12-25 16:10:45 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
 
-  // down.
-  let days = 299;
-  let hours = 10;
-  let minutes = 13;
-  let seconds = 37;
+const loveDay = new Date(2018, 1, 27).getTime();
+
+// countdown
+let timer = setInterval(function() {
+
+  // get today's date
+  const today = new Date().getTime();
+
+  // get the difference
+  const diff = today - loveDay;
+
+  // math
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
   // display
   document.getElementById("timer").innerHTML =
@@ -16,3 +27,5 @@
 <div class=\"seconds\"> \
   <div class=\"numbers\">" + seconds + "</div>seconds</div> \
 </div>";
+
+}, 1000);
